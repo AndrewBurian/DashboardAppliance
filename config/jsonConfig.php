bf81fffd8498f88358ba25d88246baf90411629a
AndrewBurian <andrew@burian.ca>
2014-05-06 10:24:50 -0700

    Updated JSON config for current data structure.

--- config/jsonConfig.php
+++ config/jsonConfig.php
@@ -8,9 +8,16 @@
  * @return  array               An array of dashboard id's
  */
 function getDashboardList($clientId){
-    $string = file_get_contents('/data/clients.json');
-    $data = json_decode($string, true);
-    return $data[$clientId];
+    $string = file_get_contents('data/clients.json');
+    $data = json_decode($string, TRUE);
+    
+    foreach($data['clients'] as $client){
+        if($client['id'] == $clientId){
+            return $client['dashboards'];
+        }
+    }
+    
+    return null;
 }
 
 /**
@@ -21,9 +28,9 @@ function getDashboardList($clientId){
  * @return  array                   An array of widget id's
  */
 function getWidgetList($dashboardId){
-    $string = file_get_contents('/data/dashboards/' . $dashboardId . '.json');
+    $string = file_get_contents('data/dashboards/' . $dashboardId . '.json');
     $data = json_decode($string, true);
-    return $data[$dashboardId];
+    return $data['widgets'];
 }
 
 /**
@@ -34,7 +41,7 @@ function getWidgetList($dashboardId){
  * @return  array                   An array of widget properties
  */
 function getWidget($widgetId){
-    $string = file_get_contents('/data/widgets/' . $widgetId . '.json');
+    $string = file_get_contents('data/widgets/' . $widgetId . '.json');
     $data = json_decode($string, true);
-    return $data[$widgetId];
+    return $data;
 }

370956b78ca8c4586fff20274797f5ffddd99cba
roropoh <hsiehrobin@gmail.com>
2014-05-07 10:41:11 -0700

    adding heatmap Widget

---
+++ views/heatmapWidget.php
@@ -0,0 +1,34 @@
+<!-- 
+    data - an array of pointsman
+    id - the name of the data displayed
+    key - the google maps API key
+-->
+<div id="{id}" style="width: 100%;height: 100%;">
+
+</div>
+
+<script src="http://maps.googleapis.com/maps/api/js?key={key}&sensor=false"></script>
+<script>
+    function initialize()
+    {
+        var mapProp = {
+            center: new google.maps.LatLng(51.508742, -0.120850),
+            zoom: 5,
+            mapTypeId: google.maps.MapTypeId.HEATMAP
+        };
+        var mainMap = new google.maps.Map(document.getElementById("{id}")
+                , mapProp);
+    }
+
+    var locations = {data};
+    
+    
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: mainMap,
+      title: ''
+    });
+
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+</script>

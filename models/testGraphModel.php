4c0c481aa7eb93e9801f22e8212dc3af71d4408c
opk1 <dsathanielle@gmail.com>
2014-05-08 03:51:32 +0800

    updated graph and testing map

--- models/testGraphModel.php
+++ models/testGraphModel.php
@@ -9,6 +9,16 @@ class testGraphModel extends baseModel {
 		$params = array();
 		$params['title'] = "testTextModel Title";
 		$params['value'] = 50;
+                
+                $params['t0'] = "jan";
+                $params['t1'] = "feb";
+                $params['t2'] = "mar";
+                $params['t3'] = "apr";
+                $params['t4'] = "may";
+                $params['t5'] = "jun";
+                $params['t6'] = "jul";
+                $params['t7'] = "aug";
+                $params['t8'] = "sept";
 		
 		return $params;
 		

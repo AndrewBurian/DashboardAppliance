3bb6fa686be47031b7f78939f974f600ffc2a392
msiwoski <siwoski.mateusz@gmail.com>
2014-05-12 11:26:04 -0700

    added a background to twitter

--- views/twitterWidget.php
+++ views/twitterWidget.php
@@ -1,4 +1,4 @@
-<div style="background-color: #ff9618;width: 100%;height: 100%;margin-left: auto;margin-right: auto;">
+<div style="background-image: url(data/images/bgTwitter.png); background-size: 100% 100%; width: 100%;height: 100%;margin-left: auto;margin-right: auto;">
     <h2 class="title" data-bind="title">{title}</h2>
     <img src="{profile_img}" /> {screen_name}
     <h3 data-bind="text | raw">{text}</h3>

366e27d3b71b9a05e0febc2e1df31fe86fd0a954
msiwoski <siwoski.mateusz@gmail.com>
2014-05-13 09:40:41 -0700

    Fixed the twitter widget a bit more

--- views/twitterWidget.php
+++ views/twitterWidget.php
@@ -1,6 +1,7 @@
-<div id="numberWidget" style="background-image: url(data/images/bgTwitter.png); ">
-    <h2 class="title" data-bind="title">{title}</h2>
-    <img src="{profile_img}" /> {screen_name}
-    <h3 data-bind="text | raw">{text}</h3>
-    <span id="twitterTimeTag" data-bind="text | raw">Last tweeted at {time}</span>
+<div id="twitterWidget" style="background-image: url(data/images/bgTwitter.png); ">
+    <h2 id="twitterTitleTag" class="title">{title}</h2>
+    <img id="twitterNameAndPictureTag" src="{profile_img}" /> {screen_name}
+   
+    <p id="twitterTweetTag">{text}</p>
+     <span id="twitterTimeTag">Last tweeted at {time}</span>
 </div>

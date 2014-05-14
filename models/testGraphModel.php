8c02a32dc540c8dcfc5343be602dc4c1af8b9a12
opk1 <dsathanielle@gmail.com>
2014-05-15 01:12:45 +0800

    fix graph yaxiis

--- models/testGraphModel.php
+++ models/testGraphModel.php
@@ -7,46 +7,51 @@ class testGraphModel extends baseModel {
     function getData() {
 
         $curval = rand(0, 620);
-        $nlines = 4;
+        $nlines = 4; // number of lines displayed on the graph
         $datapoints = 11;
 
+        /* x values to loop through */
         $xVal = array("mon", "tue", "wed", "thu", "fri", "sat", "sun");
 
         $params = array();
         $params['title'] = "Tons Collected in 2013";
-        $params['value'] = (int)((($curval/620) * 50) * (-1) + 50);
+        $params['moreinfo'] = "";
+        $params['value'] = (int)((($curval/620) * 50) * (-1) + 50); // current value displayed
 
+        /* create values and position for the x-axis */
         $params['xAxis'] = "";
         for($a = 0; $a < $datapoints; ++$a){
             $textparams['xP'] = $xVal[$a % 7];
             $textparams['xV'] = ($a * 87.142) + 5;
-            $params['xAxis'] .= parse($textparams, "graphText.php");
+            $params['xAxis'] .= parse($textparams, "graphXAxis.php");
         }
 
-        $params['y0'] = "10";
-        $params['y1'] = "20";
-        $params['y2'] = "30";
-        $params['y3'] = "40";
+        /* create values and position for the y-axis */
+        $params['yAxis'] = "";
+        for($a = 0; $a < 8; ++$a){
+            $textparams['yP'] = 80 - ($a * 10);
+            $textparams['yV'] = ($a * 74.258) + 25;
+            $params['yAxis'] .= parse($textparams, "graphYAxis.php");
+        }
 
         $params['data'] = "";
-
         for($i = 0; $i < $nlines; ++$i){
             $lineparams = array();
-            $lineparams['colour'] = '#' . dechex(rand(0,16777215));
+            $lineparams['colour'] = '#' . dechex(rand(0,1000)); // set the colour for the line
 
             $points = array();
             for($j = 0; $j < $datapoints + 1; ++$j){
-                $points[] = $j * (100/$datapoints) . "%";
-                $points[] = rand(0, 620);
+                $points[] = $j * (100/$datapoints) . "%"; // x coordinate
+                $points[] = rand(0, 620); // JSON DATA HERE (y coordinate)
             }
 
-            if (count($points) > 4) {
+            if (count($points) > 4) { // make sure there are x and y coordinates of at least two points
                 for ($k = 0; $k < (count($points) - 3); $k += 2) {
                     $lineparams['Px0'] = $points[$k];
                     $lineparams['Py0'] = $points[$k + 1];
                     $lineparams['Px1'] = $points[$k + 2];
                     $lineparams['Py1'] = $points[$k + 3];
-
+                    /* create a html line with the coordinates */
                     $params['data'] .= parse($lineparams, "graphLine.php");
                 }
             }
--- views/graphWidget.php
+++ views/graphWidget.php
@@ -1,18 +1,15 @@
-<div data-id="convergence" data-title="Convergence" style="background-color:#f1e2ff; height: 100%; width: 100%;" class="widget widget-graph convergence rickshaw_graph"><h1 class="title" data-bind="title" style="text-align: center;">{title}</h1>
+<div style="background-color:#B2008B; height: 100%; width: 100%;" class="widget widget-graph convergence rickshaw_graph"><h1 class="title" style="text-align: center;">{title}</h1>
 
-    <h2 class="value" data-bind="current | prettyNumber | prepend prefix" style="text-align: center;">{value}</h2>
+    <h2 class="value" style="text-align: center;">{value}</h2>
 
-    <p class="more-info" data-bind="moreinfo"></p>
+    <p class="more-info">{moreinfo}</p>
     <svg style="width: 100%;">
     
     {data}
     
     <g class="y_ticks plain"><g transform="translate(0,360)" style="opacity: 1;"><line class="tick" x2="4" y2="0"></line><text x="7" y="0" dy=".32em" text-anchor="start"></text></g>
 
-    <g transform="translate(0,285.74257425742576)" style="opacity: 1;"><line class="tick" x2="4" y2="0"></line><text x="7" y="0" dy=".32em" text-anchor="start">{y0}</text></g>
-    <g transform="translate(0,211.4851485148515)" style="opacity: 1;"><line class="tick" x2="4" y2="0"></line><text x="7" y="0" dy=".32em" text-anchor="start">{y1}</text></g>
-    <g transform="translate(0,137.22772277227722)" style="opacity: 1;"><line class="tick" x2="4" y2="0"></line><text x="7" y="0" dy=".32em" text-anchor="start">{y2}</text></g>
-    <g transform="translate(0,62.97029702970298)" style="opacity: 1;"><line class="tick" x2="4" y2="0"></line><text x="7" y="0" dy=".32em" text-anchor="start">{y3}</text></g>
+    {yAxis}
 
     <path class="domain" d="M4,0H0V360H4"></path></g><g class="y_grid"><g transform="translate(0,360)" style="opacity: 1;"><line class="tick" x2="610" y2="0"></line><text x="613" y="0" dy=".32em" text-anchor="start"></text></g>
 

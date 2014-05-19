340dc682a7ed83b0a2061493c1c3a0721663a847
msiwoski <siwoski.mateusz@gmail.com>
2014-05-19 16:04:12 -0700

    mapmodel mods

--- models/mapModel.php
+++ models/mapModel.php
@@ -6,14 +6,16 @@ require_once 'helpers/recollectAPI.php';
 class mapModel extends baseModel {
 
     function getData() {
-
-        $mdata = getRecollectSearches('vancouver');
-
-        for ($i = 0; $i < count($mdata); ++$i){
-           $location = $mdata[$i]['location'];
-           
-        }   
         $params = array();
+//        $mdata = getRecollectSearches('vancouver');
+//        
+//        for ($i = 0; $i < count($mdata); ++$i){
+//           $location = $mdata[$i]['location'];
+//           
+//        }   
+        $params['zoom'] = 10;
+        $params['Lat']= 51.508742;
+        $params['Long'] = -0.120850;
 
         return $params;
     }
